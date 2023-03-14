e781c84b8daee888a502cb660b2f5a23001f9fc0
HuYX <1574551212@qq.com>
2023-03-14 15:59:10 +0800

    feat 203.移除链表元素

---
+++ Algorithm/LinkList/src/main/java/com/xin/virtual/Solution203.java
@@ -0,0 +1,34 @@
+package com.xin.virtual;
+
+
+import com.xin.ListNode;
+
+/**
+ * @author Hu·YX
+ * @Description 203.移除链表元素
+ * @Link <a href="https://leetcode.cn/problems/remove-linked-list-elements/">203.移除链表元素</a>
+ * @Date 2023/03/03
+ */
+public class Solution203 {
+    public ListNode removeElements(ListNode head, int val) {
+        // 处理链表头的值等于val的节点
+        while (head != null && head.val == val) {
+            head = head.next;
+        }
+        if (head == null) {
+            return null;
+        }
+        ListNode prev = head;
+        ListNode node = head.next;
+
+        while (node != null) {
+            if (node.val == val) {
+                prev.next = node.next;
+            } else {
+                prev = node;
+            }
+            node = node.next;
+        }
+        return head;
+    }
+}
