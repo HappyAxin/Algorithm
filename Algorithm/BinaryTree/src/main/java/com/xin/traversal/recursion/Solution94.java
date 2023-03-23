02b9486d0518b2f730cc4294f306816a18b4c18c
HuYX <1574551212@qq.com>
2023-03-23 17:21:51 +0800

    feat 94. 二叉树的中序遍历

---
+++ Algorithm/BinaryTree/src/main/java/com/xin/traversal/recursion/Solution94.java
@@ -0,0 +1,35 @@
+package com.xin.traversal.recursion;
+
+import com.xin.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * @author Hu·YX
+ * @Description 94. 二叉树的中序遍历
+ * @Link <a href="https://leetcode.cn/problems/binary-tree-inorder-traversal/">94. 二叉树的中序遍历</a>
+ * @Date 2023/03/23
+ */
+public class Solution94 {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+        // 当栈不为空或当前节点不为空时，循环处理节点
+        while (curr != null || ! stack.isEmpty()) {
+            // 先将左子节点依次入栈
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            // 弹出栈顶节点并将其值加入结果集中
+            curr = stack.pop();
+            result.add(curr.val);
+            // 处理右子节点
+            curr = curr.right;
+        }
+        return result;
+    }
+}
