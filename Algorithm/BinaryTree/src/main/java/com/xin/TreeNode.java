c587fac6ca0f422fdb141613557fc523eb02631e
HuYX <1574551212@qq.com>
2023-03-23 16:45:05 +0800

    feat 二叉树

---
+++ Algorithm/BinaryTree/src/main/java/com/xin/TreeNode.java
@@ -0,0 +1,21 @@
+package com.xin;
+
+/**
+ * @author Hu·YX
+ * @Description 二叉树
+ * @Date 2023/03/23
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) {
+        this.val = val;
+    }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
