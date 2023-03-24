8d30f64ccc78c861125a7d5b01215d130c763c79
HuYX <1574551212@qq.com>
2023-03-24 10:45:00 +0800

    feat 144. 二叉树的前序遍历(递归)

--- Algorithm/BinaryTree/src/main/java/com/xin/traversal/recursion/Solution144.java
+++ Algorithm/BinaryTree/src/main/java/com/xin/traversal/recursion/Solution144.java
@@ -4,7 +4,6 @@ import com.xin.TreeNode;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Stack;
 
 /**
  * @author Hu·YX
@@ -14,29 +13,17 @@ import java.util.Stack;
  */
 public class Solution144 {
     public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> result = new ArrayList<>();
-        if (root == null) {
-            return result;
+        List<Integer> res = new ArrayList<>();
+        preorderHelper(root, res);
+        return res;
+    }
+
+    private void preorderHelper(TreeNode node, List<Integer> res) {
+        if (node == null) {
+            return;
         }
-        // 创建栈模拟递归过程
-        Stack<TreeNode> stack = new Stack<>();
-        // 将根节点入栈
-        stack.push(root);
-        // 当栈不为空时，循环处理节点
-        while (! stack.isEmpty()){
-            // 弹出栈顶节点
-            TreeNode node = stack.pop();
-            // 将节点值加入结果集
-            result.add(node.val);
-            // 将右节点入栈，保证优先处理左节点
-            if (node.right != null) {
-                stack.push(node.right);
-            }
-            // 将左节点入栈
-            if (node.left != null) {
-                stack.push(node.left);
-            }
-        }
-        return result;
+        res.add(node.val);
+        preorderHelper(node.left, res);
+        preorderHelper(node.right, res);
     }
 }
