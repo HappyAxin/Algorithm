8313291ebad04207b3248d4c8c1d7543249076e6
HuYX <1574551212@qq.com>
2023-03-24 10:44:43 +0800

    feat 94. 二叉树的中序遍历(递归)

--- Algorithm/BinaryTree/src/main/java/com/xin/traversal/recursion/Solution94.java
+++ Algorithm/BinaryTree/src/main/java/com/xin/traversal/recursion/Solution94.java
@@ -4,7 +4,6 @@ import com.xin.TreeNode;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Stack;
 
 /**
  * @author Hu·YX
@@ -14,22 +13,17 @@ import java.util.Stack;
  */
 public class Solution94 {
     public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> result = new ArrayList<>();
-        Stack<TreeNode> stack = new Stack<>();
-        TreeNode curr = root;
-        // 当栈不为空或当前节点不为空时，循环处理节点
-        while (curr != null || ! stack.isEmpty()) {
-            // 先将左子节点依次入栈
-            while (curr != null) {
-                stack.push(curr);
-                curr = curr.left;
-            }
-            // 弹出栈顶节点并将其值加入结果集中
-            curr = stack.pop();
-            result.add(curr.val);
-            // 处理右子节点
-            curr = curr.right;
+        List<Integer> res = new ArrayList<>();
+        inorderHelper(root, res);
+        return res;
+    }
+
+    private void inorderHelper(TreeNode node, List<Integer> res) {
+        if (node == null) {
+            return;
         }
-        return result;
+        inorderHelper(node.left, res);
+        res.add(node.val);
+        inorderHelper(node.right, res);
     }
 }
