c55faeb8952ec1c4b4e0463d118cb0de8ec47155
HuYX <1574551212@qq.com>
2023-04-14 09:56:36 +0800

    feat 617.合并二叉树

---
+++ Algorithm/BinaryTree/src/main/java/com/xin/modificationAndConstruction/Solution617.java
@@ -0,0 +1,25 @@
+package com.xin.modificationAndConstruction;
+
+import com.xin.TreeNode;
+
+/**
+ * @author Hu·YX
+ * @Description <a href="https://leetcode.cn/problems/merge-two-binary-trees/">617. 合并二叉树</a>
+ * @Date 2023/04/14
+ */
+public class Solution617 {
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 == null) {
+            return root2;
+        }
+
+        if (root2 == null) {
+            return root1;
+        }
+
+        TreeNode mergeNode = new TreeNode(root1.val + root2.val);
+        mergeNode.left = mergeTrees(root1.left, root2.left);
+        mergeNode.right = mergeTrees(root1.right, root2.right);
+        return mergeNode;
+    }
+}
