77558b2e57e350b8364ef94cb0d42d0836e2d57e
HuYX <1574551212@qq.com>
2023-05-17 10:32:27 +0800

    feat 376. 摆动序列

--- Algorithm/Greedy/src/main/java/com/xin/medium/Solution376.java
+++ Algorithm/Greedy/src/main/java/com/xin/medium/Solution376.java
@@ -7,29 +7,28 @@ package com.xin.medium;
  */
 public class Solution376 {
     public int wiggleMaxLength(int[] nums) {
-        int n = nums.length;
-        if (n < 2) {
-            // 如果数组长度小于2，则整个数组本身就是一个摆动子序列，直接返回数组长度
-            return n;
-        }
-        // 记录上升摆动序列的长度
-        int up = 1;
-        // 记录下降摆动序列的长度
-        int down = 1;
-
-        for (int i = 1; i < n; i++) {
-            // 如果当前数字大于前一个数字，说明形成了上升摆动，此时更新上升摆动序列的长度
-            if (nums[i] > nums[i - 1]) {
-                up = down + 1;
-            }
-            // 如果当前数字小于前一个数字，说明形成了下降摆动，此时更新下降摆动序列的长度
-            else if (nums[i] < nums[i - 1]) {
-                down = up + 1;
-            }
-            // 如果当前数字与前一个数字相等，则不改变上升和下降摆动序列的长度
+        if (nums.length < 2) {
+            // 如果数组长度小于2，直接返回数组长度
+            return nums.length;
         }
 
-        // 返回上升和下降摆动序列长度的较大值
-        return Math.max(up, down);
+        // 计算第一个差值
+        int prevDiff = nums[1] - nums[0];
+        // 如果第一个差值不为0，最长子序列长度至少为2，否则为1
+        int maxLength = prevDiff != 0 ? 2 : 1;
+
+        for (int i = 2; i < nums.length; i++) {
+            // 计算当前差值
+            int currDiff = nums[i] - nums[i - 1];
+
+            // 如果当前差值和前一个差值正负号不同，则说明形成了摆动，最长子序列长度加一
+            if ((currDiff > 0 && prevDiff <= 0) || (currDiff < 0 && prevDiff >= 0)) {
+                maxLength++;
+                // 更新前一个差值
+                prevDiff = currDiff;
+            }
+        }
+
+        return maxLength;
     }
 }
