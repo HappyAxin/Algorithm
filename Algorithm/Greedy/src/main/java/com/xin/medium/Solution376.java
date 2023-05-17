db394f615df6935f2b501c2eaad2fdf62f7d18bb
HuYX <1574551212@qq.com>
2023-05-17 10:01:44 +0800

    refactor 376. 摆动序列

--- Algorithm/Greedy/src/main/java/com/xin/medium/Solution376.java
+++ Algorithm/Greedy/src/main/java/com/xin/medium/Solution376.java
@@ -9,11 +9,13 @@ public class Solution376 {
     public int wiggleMaxLength(int[] nums) {
         int n = nums.length;
         if (n < 2) {
-            return n;  // 如果数组长度小于2，则整个数组本身就是一个摆动子序列，直接返回数组长度
+            // 如果数组长度小于2，则整个数组本身就是一个摆动子序列，直接返回数组长度
+            return n;
         }
-
-        int up = 1;  // 记录上升摆动序列的长度
-        int down = 1;  // 记录下降摆动序列的长度
+        // 记录上升摆动序列的长度
+        int up = 1;
+        // 记录下降摆动序列的长度
+        int down = 1;
 
         for (int i = 1; i < n; i++) {
             // 如果当前数字大于前一个数字，说明形成了上升摆动，此时更新上升摆动序列的长度
