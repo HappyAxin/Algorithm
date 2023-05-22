672aef52050a93c8c4f2a2dcb53727555a40f0ad
HuYX <1574551212@qq.com>
2023-05-22 10:43:53 +0800

    feat 279. 完全平方数

---
+++ Algorithm/09-DynamicProgramming/src/main/java/com/xin/backpack/Solution279.java
@@ -0,0 +1,24 @@
+package com.xin.backpack;
+
+import java.util.Arrays;
+
+/**
+ * @author Hu·YX
+ * @Description <a href="https://leetcode.cn/problems/perfect-squares/">279. 完全平方数</a>
+ * @Date 2023/05/22
+ */
+public class Solution279 {
+    public int numSquares(int n) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+
+        return dp[n];
+    }
+}
