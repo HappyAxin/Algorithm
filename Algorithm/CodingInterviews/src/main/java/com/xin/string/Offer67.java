51d36167a1f5f4dc8c9596ab023422dd3375cd12
HuYX <1574551212@qq.com>
2023-05-30 13:59:06 +0800

    feat 剑指 Offer 67. 把字符串转换成整数

--- Algorithm/CodingInterviews/src/main/java/com/xin/string/Offer67.java
+++ Algorithm/CodingInterviews/src/main/java/com/xin/string/Offer67.java
@@ -11,29 +11,29 @@ public class Offer67 {
         int result = 0;
 
         // 如果字符串为空，返回0
-        if(length == 0) {
+        if (length == 0) {
             return 0;
         }
 
         // 忽略前面的空格
-        while(i < length && str.charAt(i) == ' ') {
+        while (i < length && str.charAt(i) == ' ') {
             i++;
         }
 
         // 如果字符串只包含空格，则返回0
-        if(i == length) {
+        if (i == length) {
             return 0;
         }
 
         // 检查第一个非空格字符是否为符号
-        if(str.charAt(i) == '-' || str.charAt(i) == '+') {
-            sign = (str.charAt(i++) == '-') ? -1 : 1;
+        if (str.charAt(i) == '-' || str.charAt(i) == '+') {
+            sign = (str.charAt(i++) == '-') ? - 1 : 1;
         }
 
         // 转换数字
-        while(i < length && str.charAt(i) >= '0' && str.charAt(i) <= '9') {
+        while (i < length && str.charAt(i) >= '0' && str.charAt(i) <= '9') {
             // 检查是否越界
-            if(result > Integer.MAX_VALUE / 10 || (result == Integer.MAX_VALUE / 10 && str.charAt(i) - '0' > Integer.MAX_VALUE % 10)) {
+            if (result > Integer.MAX_VALUE / 10 || (result == Integer.MAX_VALUE / 10 && str.charAt(i) - '0' > Integer.MAX_VALUE % 10)) {
                 return (sign == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
             }
 
